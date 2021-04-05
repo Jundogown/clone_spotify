1b103cbacffaefc41523aa234b1ebe40f0c5e57b
jundogown <ns8708ns@naver.com>
2021-04-05 20:02:42 +0900

    [UPDATA] Main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Login from './components/Login'
+import Login from './components/Login';
 import { getTokenFromResponse } from './components/spotify';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Main from './components/Main';
 
 const App = () => {
     const [token, setToken] = useState(null);
@@ -12,10 +14,20 @@ const App = () => {
         const token = hash.access_token;
 
         console.log('token', token);
+
+        const userManager = {
+          token : token,
+        };
+
+        console.log(userManager);
+
+        setToken(token);
+        
     }, []);
     return(
         <div className="login">
-            <Login/>
+          {!token && <Login/>}
+          {token && <Main/>}
         </div>
     )
 }
