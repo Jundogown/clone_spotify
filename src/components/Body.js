2bccae16195cf4a1923610b68c78f5d5446ed835
jundogown <ns8708ns@naver.com>
2021-04-06 13:43:00 +0900

    [ADD] Body

---
+++ src/components/Body.js
@@ -0,0 +1,21 @@
+import react from 'react';
+import styled from 'styled-components';
+
+const BodyContainer = styled.div`
+    background: gray;
+    height: 100vh;
+    ${'' /* flex: 70%; */}
+    width: 167vh;
+`;
+
+const Body = () => {
+    return(
+        <div>
+            <BodyContainer>
+                
+            </BodyContainer>
+        </div>
+    );
+}
+
+export default Body;
