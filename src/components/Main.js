bb586609af4c44db00b9a2704e8b18e63e148864
jundogown <ns8708ns@naver.com>
2021-04-09 09:37:58 +0900

    [FIX] main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,32 @@ import styled from 'styled-components';
 import Sidebar from './Sidebar';
 import Body from './atoms/Body';
 import Footer from './atoms/Footer';
+import axios from 'axios';
 
 const MainContainer = styled.div`
     display: flex;
 `
 
 const Main = () => {
+    const token = sessionStorage.getItem('token');
+
+    console.log(token);
+    
+    axios({
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+        method: 'GET',
+        url: `https://api.spotify.com/v1/me`,
+    }).then((res) => {
+        console.log(res);
+        const userid = res.data.id;
+        sessionStorage.setItem("userId", userid);
+        
+    }).catch(error => {
+        console.log(error);
+    });
+
     return(
         <div className="player">
             <MainContainer>
