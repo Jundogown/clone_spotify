a5d4d8c8bbdc51397532d9bd93e1a8b85379c551
jundogown <ns8708ns@naver.com>
2021-04-09 09:37:34 +0900

    [ADD] line

---
+++ src/components/atoms/Line.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import styled from 'styled-components';
+
+const Hr = styled.hr`
+    border: 1px solid #282828;
+    width: 90%;
+    margin: 10px auto;
+    padding: 0;
+`;
+
+const Line = () => {
+    return(
+        <Hr/>
+    );
+}
+
+export default Line;
