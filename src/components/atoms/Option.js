63dccc710c6c88451c58b29f2d3d7a2bb9f7429e
jundogown <ns8708ns@naver.com>
2021-04-09 09:37:46 +0900

    [FIX] option

--- src/components/atoms/Option.js
+++ src/components/atoms/Option.js
@@ -1,18 +1,35 @@
+import { IconButton } from '@material-ui/core';
 import react from 'react';
 import styled from 'styled-components';
 
 const SidebarOption = styled.div`
-    color: #ffffff;
-    padding-left: 24px;
-    font-size: ;
+    display: flex;
+    color: #949699;
+    align-items: center;
+    cursor: pointer;
+    padding-left: 18px;
+    transition: 200ms color ease-in;
+    &:hover {
+        color: #ffffff;
+    }
 `
 
-const Option = ({ title, icon }) => {
+const Span = styled.span`
+    margin-top: 8px;
+    margin-left: 2px;
+    font-size: 14px;
+`;
+
+const Icon = styled.div`
+    padding-left: 10px;
+    padding-right: 10px;
+`;
+
+const Option = ({ title , Icon }) => {
     return(
         <SidebarOption>
-            <span>
-                {title}
-            </span>
+            {Icon && <Icon/>}
+            {Icon ? <h4>{title}</h4> : <Span>{title}</Span>}
         </SidebarOption>
     );
 }
