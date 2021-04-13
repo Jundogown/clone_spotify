33d1c51fe144844b1a088dccc0b0f80cdb86cd7f
jundogown <ns8708ns@naver.com>
2021-04-13 19:41:22 +0900

    [FIX] Search

--- src/components/Organisms/atoms/Search.js
+++ src/components/Organisms/atoms/Search.js
@@ -2,9 +2,15 @@ import react from 'react';
 import styled from 'styled-components';
 
 const SearchBar = styled.input`
-    width: 10vh;
-    background-color: gray;
-    height: 10vh;
+    width: 50vh;
+    height: 6vh;
+    font-size: 3vh;
+    border-radius: 100px;
+    background-color: white;
+    outline: none;
+    &:focus{
+        outline: none;
+    }
 `
 
 const Search = () => {
