59443421dd698927bdabafe343ce56e70b35a119
jundogown <ns8708ns@naver.com>
2021-04-13 09:16:16 +0900

    [FIX] button

---
+++ src/components/atoms/Button.js
@@ -0,0 +1,37 @@
+import { IconButton } from '@material-ui/core';
+import react from 'react';
+import styled from 'styled-components';
+
+const ButtonOption = styled.button`
+    display: flex;
+    color: #949699;
+    align-items: center;
+    cursor: pointer;
+    padding-left: 18px;
+    transition: 200ms color ease-in;
+    &:hover {
+        color: #ffffff;
+    }
+`;
+
+const Span = styled.span`
+    margin-top: 8px;
+    margin-left: 2px;
+    font-size: 14px;
+`;
+
+const Icon = styled.div`
+    padding-left: 10px;
+    padding-right: 10px;
+`;
+
+const Button = ({ title , Icon }) => {
+    return(
+        <ButtonOption>
+            {Icon && <Icon/>}
+            {Icon ? <h4>{title}</h4> : <Span>{title}</Span>}
+        </ButtonOption>
+    );
+}
+
+export default Button;
