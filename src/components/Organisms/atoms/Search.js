0e7be9315b70416a1906e8e3ee7ed2fa15bca4c0
jundogown <ns8708ns@naver.com>
2021-04-13 15:36:03 +0900

    [ADD] Search

---
+++ src/components/Organisms/atoms/Search.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import styled from 'styled-components';
+
+const SearchBar = styled.input`
+    width: 10vh;
+    background-color: gray;
+    height: 10vh;
+`
+
+const Search = () => {
+    return(
+        <SearchBar>
+        </SearchBar>
+    )
+}
+
+export default Search;
