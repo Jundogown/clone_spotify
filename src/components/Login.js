0b42b8b9cc7168389766b5765e56cfb97ead8e92
jundogown <ns8708ns@naver.com>
2021-04-13 15:34:08 +0900

    [FIX] Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { accessUrl } from './atoms/spotify';
+import { accessUrl } from './spotify';
 
 const LoginBackground = styled.div`
     display: grid;
