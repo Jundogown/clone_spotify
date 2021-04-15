0f6a8d911ce1752f3059278fc9ef4d9ab53a81c5
jundogown <ns8708ns@naver.com>
2021-04-15 19:58:53 +0900

    [FIX] Search

--- src/components/Organisms/atoms/Search.js
+++ src/components/Organisms/atoms/Search.js
@@ -1,11 +1,16 @@
-import react from 'react';
+import axios from 'axios';
+import react, { useState } from 'react';
+import './Search.css';
 import styled from 'styled-components';
+import Select from 'react-select'
 
 const SearchBar = styled.input`
     width: 50vh;
-    height: 6vh;
-    font-size: 3vh;
+    height: 3.5vh;
+    font-size: 1.8vh;
     border-radius: 100px;
+    border: none;
+    padding-left: 20px;
     background-color: white;
     outline: none;
     &:focus{
@@ -13,10 +18,75 @@ const SearchBar = styled.input`
     }
 `
 
+const InputArea = styled.div`
+    width: 60vh;
+    height: 4vh;
+    display: flex;
+    background-color: #ffffff;
+`
+
 const Search = () => {
+    const [ input, setInput ] = useState({
+        data: '',
+    });
+
+    const token = sessionStorage.getItem('token');
+    const { data } = input;
+
+    const onChange = (e) => {
+        const { value, data } = e.target;
+        setInput({
+          ...input,
+          [data]: value
+        });
+        console.log(value);
+        const inputData = value;
+
+        localStorage.setItem('inputData', inputData);
+    }
+    
+    const onClick = () => {
+        const inputData = localStorage.getItem('inputData');
+
+        axios({
+            headers: {  
+                "Authorization": `Bearer ${token}`,
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            method: 'GET',
+            url: 'https://api.spotify.com/v1/search',
+            params: {
+                q: inputData,
+                type: 'track',
+            },
+        }).then((res) => {
+            console.log(res);
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    // const options = [
+    //     { value: 'Artist', label: 'Artist' },
+    //     { value: 'Music', label: 'Music' },
+    //     { value: 'Album', label: 'Album' }
+    // ]
+
+    // console.log(inputData);
+
     return(
-        <SearchBar>
-        </SearchBar>
+        <div>
+            <InputArea>
+                <select name="menu">
+                    <option value="Artist">Artist</option>
+                    <option value="Music">Music</option>
+                    <option value="Album">Album</option>
+                </select>
+                <SearchBar onChange={onChange} placeholder="Artist, Music..." />
+                <button className="btn" onClick={onClick}></button>
+            </InputArea>
+        </div>
     )
 }
 
