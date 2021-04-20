0ad6c09e59a5a1b5ac572163832d20d012375ccd
jundogown <ns8708ns@naver.com>
2021-04-20 11:26:49 +0900

    [ADD] VerticalLine
    추후 ui구성에 사용할 세로선을 추가 하였습니다,.

---
+++ src/components/Organisms/atoms/VerticalLine.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import styled from 'styled-components';
+
+const VerLine = styled.div`
+    border-left: 6px solid green;
+    height: 500px;
+`
+
+const VerticalLine = () => {
+    return(
+        <VerLine/>
+    );
+}
+
+export default VerticalLine;
