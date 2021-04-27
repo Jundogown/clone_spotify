4fb7c20e4278634c566b9deef21a530ca7bd8062
jundogown <ns8708ns@naver.com>
2021-04-27 21:02:45 +0900

    [FIX] Footer
    음악을 재생하는 플레이 바를 담고있는 Footer 입니다.

--- src/components/Organisms/Footer.js
+++ src/components/Organisms/Footer.js
@@ -1,8 +1,15 @@
 import react from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipNextIocn from '@material-ui/icons/SkipNext';
+import RepeatIcon from '@material-ui/icons/Repeat';
 
 const FooterContainer = styled.div`
     position: fixed;
+    display: flex;
+    justify-content: space-between;
     bottom: 0;
     height: 65px;
     width: 100%;
@@ -10,9 +17,41 @@ const FooterContainer = styled.div`
     padding: 15px;
 `;
 
+const FooterLeft = styled.div`
+    flex: 0.3;
+`;
+
+const FooterCenter = styled.div`
+    flex: 0.5;
+    padding: 0 100px;
+    color: white;
+    display: felx;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 300px;
+`;
+
+const FooterRight = styled.div`
+    flex: 0.25;
+`;
+
 const Footer = () => {
     return(
-        <FooterContainer/>
+        <FooterContainer>
+            <FooterLeft>
+                <span>track name</span>
+            </FooterLeft>
+            <FooterCenter>
+                <ShuffleIcon/>
+                <SkipPreviousIcon/>
+                <PlayCircleOutlineIcon/>
+                <SkipNextIocn/>
+                <RepeatIcon/>
+            </FooterCenter>
+            <FooterRight>
+                <Grid></Grid>
+            </FooterRight>
+        </FooterContainer>
     )
 }
 
