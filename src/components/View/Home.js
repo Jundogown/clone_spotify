4186419f27301901a5512dc01bdbc9011823b8f2
jundogown <ns8708ns@naver.com>
2021-05-03 19:24:46 +0900

    [FIX] HOME

--- src/components/View/Home.js
+++ src/components/View/Home.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
 const Home =() => {
-
+    const token = sessionStorage.getItem('token');
+    
+    axios({
+        headers:{
+            "Authorization": `Bearer ${token}`
+        },
+        method: 'GET',
+        url: '',
+    }).then((res) => {
+        console.log(res);
+    }).catch(error => {
+        console.log(error);
+    })
 }
 
 export default Home;
